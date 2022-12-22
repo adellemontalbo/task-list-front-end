bd88a46e712312704eb19f488ad30452a4df181c
Adelle Montalbo <adellemontalbo@gmail.com>
2022-12-21 18:55:28 -0800

    added proptype to newtaskform, now input working, but task doesn't appear until page refreshed

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const kDefaultFormState = {
   description: '',
@@ -51,3 +52,8 @@ const NewTaskForm = ({ handleTaskSubmit }) => {
 };
 
 export default NewTaskForm;
+
+NewTaskForm.propTypes = {
+  handleTaskSubmit: PropTypes.func.isRequired
+};
+
